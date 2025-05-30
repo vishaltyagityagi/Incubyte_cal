99f3bdc3b7be9e50a8bae4d9b6d6172f0cd7ed52
Vishal Tyagi <vishal.tyagi@builder.ai>
2025-05-30 14:33:13 +0530

    refactor(spec_class): #[0003] spec modified

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator, type: :lib do
-  subject { described_class }
+  let(:calculator) { StringCalculator.new }
+
   describe '.add' do
     context 'when input is empty' do
       it 'returns 0' do
-        expect(described_class.new.add("")).to eq(0)
+        expect(calculator.add("")).to eq(0)
       end
     end
   end
