c3e66bf4a02718983f97e422eefe661377bdff99
Vishal Tyagi <vishal.tyagi@builder.ai>
2025-05-30 13:48:51 +0530

    impr(rspec_setup): #[0001] setup rspec config

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 #including gem
 
 gem 'minitest'
+gem 'rspec-rails'
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require_relative '../lib/string_calculator'
+
+RSpec.describe StringCalculator do
+
+end
