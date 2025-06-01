f12cf16b711afe004ecb7086d0bb3ba25acf4cda
Vishal Tyagi <vishal.tyagi@builder.ai>
2025-06-01 14:15:13 +0530

    refactor(cal_klass): #[0004] added another test for plan string including multi char without any special char

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe StringCalculator, type: :lib do
         expect(calculator.add("1")).to eq(1)
       end
 
+      it "returns the sum of two numbers" do
+        expect(calculator.add("1,5")).to eq(6)
+      end
+
     end
   end
 
