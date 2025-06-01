f87003c1b6fd00d3dae821ca6252d37bcc735c33
Vishal Tyagi <vishal.tyagi@builder.ai>
2025-06-01 14:44:47 +0530

    refactor(cal_klass): #[0004] enhance code for negative numbers str

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,11 +4,15 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
     @numbers = string_to_numbers(string)
-    @numbers.inject(0){|defaut, actual| defaut + actual}
+    @numbers.inject(0) { |defaut, actual| defaut + actual }
   end
 
-  #extract numbers for string using regex
+  # extract numbers for string using regex
   def string_to_numbers(string)
+    negative_numbers = string.scan(/-\d+/)
+    unless negative_numbers.empty?
+      raise "Negative numbers not allowed: #{negative_numbers.join(',')}"
+    end
     string.scan(/-?\d+/).map(&:to_i)
   end
 
