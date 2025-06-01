0483e72b7cc936e969759f657d4aea35217e6548
Vishal Tyagi <vishal.tyagi@builder.ai>
2025-06-01 14:13:43 +0530

    refactor(cal_klass): #[0005] enhanced function to access no nil string with values without special chars

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,13 @@ class StringCalculator
 
   def add(string)
     return 0 if string.empty?
+    @numbers = string_to_numbers(string)
+    @numbers.inject(0){|defaut, actual| defaut + actual}
   end
-  
+
+  #extract numbers for string using regex
+  def string_to_numbers(string)
+    string.scan(/-?\d+/).map(&:to_i)
+  end
 
 end
